2549946f93b3d42a0a3e56e566419389225f5cb9
SoyElMatra <matramatrayt@gmail.com>
2025-04-11 17:19:14 -0300

    Ejercicio con nota Nº1

---
+++ Control_temp-Santiago-Martin.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Control_temp-Santiago-Martin.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Control_temp-Santiago-Martin.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=controltemp-santiago-martin.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/controltemp-santiago-martin.x.tar
