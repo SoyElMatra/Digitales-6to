678494caf241d0a018480de4ec82f4f0510dd6b1
SoyElMatra <matramatrayt@gmail.com>
2025-05-13 17:26:20 -0300

    RS232-Leds-Delay - ANDA

--- RS232-leds-delay.X/RS232.c
+++ RS232-leds-delay.X/RS232.c
@@ -4,12 +4,20 @@
 #use rs232(baud=9600, xmit=pin_c6, rcv=pin_c7)
 #use standard_io(B)
 
-int recvint[3] = {5, 5, 5}, i = 0, cont1 = 0, cont2 = 0, cont3 = 0;
+int recvint[3] = {5, 5, 5}, i = 0, cont = 0, cont1 = 0, cont2 = 0, cont3 = 0;
 int1 scan = 0;
 char recv;
 
 #INT_TIMER1
+
 void isr_timer1() {
+    if (scan | i >= 2) {
+        cont++;
+        if (cont == 2) {
+            cont = 0;
+            scan = 0;
+        }
+    }
     if (recvint[0] < 4) {
         cont1--;
         if (cont1 <= 0) {
@@ -49,10 +57,12 @@ void isr_timer1() {
             output_low(PIN_B2);
     }
 
+    clear_interrupt(INT_RDA);
     set_timer1(3036);
 }
 
 #INT_RDA
+
 void RDA_isr() {
     recv = getc();
     if (recv == '*') {
@@ -61,28 +71,26 @@ void RDA_isr() {
     } else if (scan) {
         if (i < 3) {
             recvint[i] = recv - 48;
-            switch(i) {
-                case 0: cont1 = recvint[i]; break;
-                case 1: cont2 = recvint[i]; break;
-                case 2: cont3 = recvint[i]; break;
+            switch (i) {
+                case 0: cont1 = recvint[i];
+                    break;
+                case 1: cont2 = recvint[i];
+                    break;
+                case 2: cont3 = recvint[i];
+                    break;
             }
             i++;
-        }
-        if (i >= 3) {
-            scan = 0;
-            i = 0;
         }
     }
 }
 
 void main() {
-    set_tris_b(0xF8);
     enable_interrupts(GLOBAL);
     enable_interrupts(INT_RDA);
     enable_interrupts(INT_TIMER1);
     setup_timer_1(T1_INTERNAL | T1_DIV_BY_8);
     set_timer1(3036);
 
-    while(TRUE) {
+    while (TRUE) {
     }
 }
