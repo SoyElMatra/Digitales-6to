3fd6f5477b1281f17d23d3b1df1254bd68836f71
SoyElMatra <santimanumartin@gmail.com>
2025-06-12 15:06:25 -0300

    contador RPM ANDA

--- Motor-Encoder.X/Motor-Encoder.c
+++ Motor-Encoder.X/Motor-Encoder.c
@@ -1,6 +1,7 @@
 #include <16F876A.h>
 #FUSES XT,NOWDT,NOLVP
 #use delay(clock=4000000)
+#use standard_io(C)
 #use standard_io(B)
 #use standard_io(A)
 #define LCD_DATA_PORTB
@@ -8,27 +9,40 @@
 #include <lcd.c>
 #include <kbd.c>
 
-int timcont, rpmcont;
-
+int16 rpmcont;
+int16 timcont;
+int1 check = 1;
 
 #INT_TIMER1
-void isr_timer1(){
-    rpmcont = ((timcont * 2) / 360) * 600; 
-    set_timer0(0);
+
+void isr_timer1() {
+    rpmcont = ((timcont * 600.0) / 360) / 2.0;
+    timcont = 0;
+    check = 1;
     set_timer1(15535);
 }
 
-void main(){
+#INT_TIMER0
+
+void isr_timer0() {
+    timcont++;
+    set_timer0(255);
+}
+
+void main() {
     setup_timer_1(T1_INTERNAL | T1_DIV_BY_4);
-    setup_timer_0(T0_EXT_H_TO_L | T0_DIV_2);
+    setup_timer_0(T0_EXT_L_TO_H | T0_DIV_1);
+    enable_interrupts(INT_TIMER0);
     enable_interrupts(INT_TIMER1);
     enable_interrupts(GLOBAL);
     set_timer1(15535);
+    set_timer0(255);
     lcd_init();
     kbd_init();
-    while(1){
-        printf(lcd_putc,"Revoluciones: \n%u", rpmcont);
-        timcont = get_timer0;
-        delay_ms(100);
+    while (1) {
+        if (check == 1) {
+            check = 0;
+            printf(lcd_putc, "\fRevoluciones: \n%lu", rpmcont);
+        }
     }
 }
