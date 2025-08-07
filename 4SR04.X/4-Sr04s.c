5b227a02b65cc8e4946342bb564e075d69f83170
SoyElMatra <matramatrayt@gmail.com>
2025-08-07 14:57:05 -0300

    laconchadesumadre, yanosepuedecomprarunkilodeosobuco

--- 4SR04.X/4-Sr04s.c
+++ 4SR04.X/4-Sr04s.c
@@ -2,8 +2,10 @@
 #fuses XT,NOWDT,PUT,NOWRT,NOLVP
 #use delay(clock= 4000000)
 #use standard_io(B)
+#use standard_io(C)
 
-#include<lcd4201.c>
+#define LCD_DATA_PORT getenv("SFR:PORTC")
+#include<lcd.c>
 
 #define TRIG1 PIN_B0
 #define TRIG2 PIN_B1
@@ -11,39 +13,42 @@
 #define TRIG4 PIN_B3
 
 int16 duration, oldDistance1, oldDistance2, oldDistance3, oldDistance4, distance1, distance2, distance3, distance4;
-int1 flag1 = 1,flag2 = 1,flag3 = 1,flag4 = 1, check = 1, checkdisp = 1;
+int1 flagdisp = 1, check = 1;
 int i = 0;
+
 #INT_RB
 
 RB_isr() {
     duration = get_timer1();
+    disable_interrupts(INT_RB);
+    check = 1;
     switch (i) {
         case 0:
             distance1 = (duration / 58) - 5;
-            if(distance1 != oldDistance1){
-                check = 1;
-                oldDistance1 = distance1
+            if (distance1 != oldDistance1) {
+                flagdisp = 1;
+                oldDistance1 = distance1;
             }
             break;
         case 1:
             distance2 = (duration / 58) - 5;
-            if(distance2 != oldDistance2){
-                check = 1;
-                oldDistance2 = distance2
+            if (distance2 != oldDistance2) {
+                flagdisp = 1;
+                oldDistance2 = distance2;
             }
             break;
         case 2:
             distance3 = (duration / 58) - 5;
-            if(distance3 != oldDistance3){
-                check = 1;
-                oldDistance3 = distance3
+            if (distance3 != oldDistance3) {
+                flagdisp = 1;
+                oldDistance3 = distance3;
             }
             break;
         case 3:
             distance4 = (duration / 58) - 5;
-            if(distance4 != oldDistance4){
-                check = 1;
-                oldDistance4 = distance4
+            if (distance4 != oldDistance4) {
+                flagdisp = 1;
+                oldDistance4 = distance4;
             }
             break;
     }
@@ -51,6 +56,7 @@ RB_isr() {
 
 void sr04dist1() {
     i = 0;
+    enable_interrupts(INT_RB);
     output_high(TRIG1);
     delay_us(10);
     output_low(TRIG1);
@@ -59,6 +65,7 @@ void sr04dist1() {
 
 void sr04dist2() {
     i = 1;
+    enable_interrupts(INT_RB);
     output_high(TRIG2);
     delay_us(10);
     output_low(TRIG2);
@@ -67,6 +74,7 @@ void sr04dist2() {
 
 void sr04dist3() {
     i = 2;
+    enable_interrupts(INT_RB);
     output_high(TRIG3);
     delay_us(10);
     output_low(TRIG3);
@@ -75,6 +83,7 @@ void sr04dist3() {
 
 void sr04dist4() {
     i = 3;
+    enable_interrupts(INT_RB);
     output_high(TRIG4);
     delay_us(10);
     output_low(TRIG4);
@@ -82,22 +91,39 @@ void sr04dist4() {
 }
 
 void main() {
-    set_tris_B(0xF0);
-    output_low(PIN_B1);
     enable_interrupts(GLOBAL);
     setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
-    delay_ms(500);
     clear_interrupt(INT_RB);
     enable_interrupts(INT_RB);
-    
+
     lcd_init();
     while (1) {
-        if(flag1 == 1 || distance1 > 25289 || distance2 > 25289 || distance3 > 25289 || distance4 > 25289){
-            
-        }
-        if(check == 1){
-            printf(lcd_putc, "Sensor1: %lu\nSensor2: %lu\nSensor3: %lu\nSensor4: %lu\n", distance1, distance2, distance3, distance4);
+        delay_ms(10);
+        if (check == 1 || duration >= 25289) {
             check = 0;
+            i++;
+            if(i >= 4){
+                i = 0;
+            }
+            switch(i){
+                case 0:
+                    sr04dist2();
+                    break;
+                case 1: 
+                    sr04dist3();
+                    break;
+                case 2: 
+                    sr04dist4();
+                    break;
+                case 3: 
+                    sr04dist1();
+                    break;
+            }
+        }
+        if (flagdisp == 1) {
+            flagdisp = 0;
+            printf(lcd_putc, "\fSR1: %lu SR2: %lu\nSR3: %lu SR4: %lu\n", distance1, distance2, distance3, distance4);
         }
     }
 }
+
